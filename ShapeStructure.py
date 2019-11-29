89859be6f266cd4580e59607e44647a866c25bba
MxFxM <32713448+MxFxM@users.noreply.github.com>
2019-11-29 22:28:01 +0100

    create shape

--- ShapeStructure.py
+++ ShapeStructure.py
@@ -4,8 +4,8 @@ import max_color as mc
 
 pygame.init()
 
-display_width = 800
-display_height = 600
+display_width = 1000
+display_height = 800
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('ShapeStructure')
@@ -17,6 +17,29 @@ y = (display_height * 0.8)
 
 done = False
 
+
+def createShape():
+    points_list = []
+
+    points_list.append([200, 200])  # a
+    points_list.append([300, 50])  # b
+    points_list.append([400, 150])  # c
+    points_list.append([350, 350])  # d
+    points_list.append([500, 450])  # e
+    points_list.append([300, 650])  # f
+    points_list.append([300, 500])  # g
+    points_list.append([250, 550])  # h
+    points_list.append([150, 450])  # i
+    points_list.append([250, 350])  # j
+    points_list.append([50, 350])  # k
+    points_list.append([100, 200])  # l
+    points_list.append([200, 250])  # m
+
+    return points_list
+
+
+shape = createShape()
+
 while not done:
     for event in pygame.event.get():  # event handling
         if event.type == pygame.QUIT:  # exit on closing
@@ -24,10 +47,8 @@ while not done:
         elif event.type == pygame.KEYDOWN and event.key == 13:  # exit on enter
             done = True
 
-    # pygame.draw.line(gameDisplay, white, [10, 10], [100, 500])  # draw line
-    # draw line
-    pygame.draw.lines(gameDisplay, mc.color('white'), True, [
-                      [10, 10], [100, 500], [200, 50]])  # lines between points and closed=True
+    # lines between points and closed=True
+    pygame.draw.lines(gameDisplay, mc.color('light_blue'), True, shape)
 
     pygame.display.update()  # draw to screen
     clock.tick(60)  # limit at 60 fps
