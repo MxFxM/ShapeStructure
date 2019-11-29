f0c38604eb6f9aadf0602d5f76bba3193dc3be9b
MxFxM <32713448+MxFxM@users.noreply.github.com>
2019-11-29 22:13:08 +0100

    added more colors

--- max_color.py
+++ max_color.py
@@ -3,3 +3,11 @@ def color(col):
         return (0, 0, 0)
     if col == 'white':
         return (255, 255, 255)
+    if col == 'red':
+        return (255, 0, 0)
+    if col == 'green':
+        return (0, 255, 0)
+    if col == 'blue':
+        return (0, 0, 255)
+    if col == 'light_blue':
+        return (0, 100, 255)
